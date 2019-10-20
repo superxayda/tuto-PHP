82356a788a254299113c1f4e6513f6768460f15c
sungvadan <svd.phan@gmail.com>
2019-10-20 11:54:31 +0200

    fixup! Les class

--- class/Creneau.php
+++ class/Creneau.php
@@ -1,24 +1,31 @@
 <?php
+class Creneau {
 
-class Creneau
-{
     public $debut;
 
     public $fin;
 
-    public function __construct(int $debut, int $fin)
+    public function __construct (int $debut, int $fin) 
     {
         $this->debut = $debut;
         $this->fin = $fin;
     }
 
-    public function inclureHeure(int $heure): bool
+    public function toHTML (): string
+    {
+        return "<strong>{$this->debut}h</strong> à <strong>{$this->fin}h</strong>";
+    }
+
+    public function inclusHeure (int $heure): bool 
     {
         return $heure >= $this->debut && $heure <= $this->fin;
     }
 
-    public function intersect(Creneau $creneau): bool
+    public function intersect (Creneau $creneau): bool
     {
-
+        return $this->inclusHeure($creneau->debut) ||
+            $this->inclusHeure($creneau->fin) ||
+            ($this->debut > $creneau->debut && $this->fin < $creneau->fin);
     }
+
 }
--- cli/test.php
+++ cli/test.php
@@ -1,9 +1,6 @@
 <?php
-require __DIR__ .'/../class/Creneau.php';
-
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Creneau.php';
 $creneau = new Creneau(9, 12);
-$creneau2 = new Creneau(12, 14);
-var_dump(
-    $creneau->inclureHeure(10),
-    $creneau2->inclureHeure(10)
-);
+$creneau2 = new Creneau(14, 16);
+$creneau->intersect($creneau2);
+echo $creneau->toHTML();
